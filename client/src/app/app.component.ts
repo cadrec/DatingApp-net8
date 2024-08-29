f0a5fcaee86c85aee3b7f6743a1aae0385838437
cadrec <ccarrigan4230@gmail.com>
2024-08-29 15:56:33 -0400

    6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,9 +7,9 @@ import { HomeComponent } from "./home/home.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
